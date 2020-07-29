ff499e21d58d710200a7fe32d7ddaa424992d961
Antony Parker <antonyqparker@gmail.com>
2020-07-29 10:07:33 +0100

    Can add books and form is now cleared on submit

--- app.js
+++ app.js
@@ -7,32 +7,45 @@ function Book(title, author, isbn) {
 // UI constructor
 function UI() {}
 UI.prototype.addBookToList = function (book) {
-  const list = document.getElementById('book-list')
-}
+  const list = document.getElementById("book-list");
+  // Create tr element
+  const row = document.createElement("tr");
+  console.log(row);
+  // Insert cols
+  row.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.isbn}</td>
+  <td><a href="#" class="delete">X</a></td>`;
 
+  list.appendChild(row);
+};
 
-
+// Clear field function
+UI.prototype.clearFields = function () {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+};
 
 // Event Listeners
-document.getElementById('book-form').addEventListener('submit', function (e) {
+document.getElementById("book-form").addEventListener("submit", function (e) {
   // Get form values
-  const title = document.getElementById('title').value,
-    author = document.getElementById('author').value,
-    isbn = document.getElementById('isbn').value;
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
 
-  // Instantiating a book 
-  const book = new Book(title, author, isbn)
+  // Instantiating a book
+  const book = new Book(title, author, isbn);
 
   // Instantiating UI
-  const ui = new UI()
+  const ui = new UI();
 
   // Add book to list
-  ui.addBookToList(book)
+  ui.addBookToList(book);
 
-
-
-
-
+  // Clear fields
+  ui.clearFields();
 
   e.preventDefault();
 });
